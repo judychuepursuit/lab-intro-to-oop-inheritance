ae21c735cd54488d1000d24353a128d45593dbd6
Judy <judychue@pursuit.org>
2023-09-24 20:40:51 -0400

    first commit

--- food.js
+++ food.js
@@ -1,4 +1,24 @@
 // Create class below
+//  class Food {
+//     constructor (name, daysToSpoil) {
+//     this.name = name
+//     this.daysToSpoil = daysToSpoil
+//     this.fresh = true
+//  }
+
+//     prepare(){
+//         console.log(`the ${this.name} is being prepared`)
+//     }
+
+//     isFresh () {
+//         console.log(
+//             this.daysToSpoil ?
+//             `There are ${ this.daysToSpoil} days left before ${this.name} spoils` : 
+//         )
+//         if (this.daysToSpoil) {
+
+//         }
+//     }
 
 // Do not edit below this line
-module.exports = Food;
+// module.exports = Food;
